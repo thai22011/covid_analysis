bd2888bdaf585385fb5b5d1a15788d756f4691c4
thai22011 <ethainguyen@gmail.com>
2023-01-30 17:35:58 -0800

    update analysis, create table, create view

--- COVID SQL 2020-2023.sql
+++ COVID SQL 2020-2023.sql
@@ -7,14 +7,18 @@ Skills used: Joins, CTE's, Temp Tables, Windows Functions, Aggregate Functions, 
 -Credit: Alex the Analyst Code Along Project 1/4
 */
 
-
+-- Check the CovidDeaths table
 SELECT *
-FROM CovidDeaths$
+FROM CovidDeaths$;
+
+-- Check the CovidVaccinations Table
+SELECT *
+FROM CovidVaccinations$
 ORDER BY location, date;
 
---SELECT *
---FROM CovidVaccinations$
---ORDER BY location, date;
+-- Check table by column name
+SELECT *
+FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = 'CovidDeaths$';
 
 -- Select Data that we are going to be starting with
 Select location, date, total_cases, new_cases, total_deaths, population
@@ -54,25 +58,87 @@ GROUP BY continent
 ORDER BY TotalDeathCount DESC;
 
 -- GLOBAL NUMBERS
-SELECT SUM(new_cases) as total_cases, SUM(cast(new_deaths as int)) as total_deaths, SUM(cast(new_deaths as int))/SUM(New_Cases)*100 as DeathPercentage
+SELECT SUM(new_cases) as total_cases, SUM(cast(new_deaths as int)) as total_deaths, SUM(cast(new_deaths as int))/SUM(new_cases)*100 as death_perc
 FROM CovidDeaths$
-WHERE continent is not null 
+WHERE continent IS NOT NULL
 ORDER BY 1,2 DESC;
 
-/* Join the two table together using OUTER JOIN
+/* Join the two table together using OUTER JOIN and Save them into a new table 'CovidDV'
 */
 
-;
+-- The following code is for MySQL, will not work for SSMS
+-- DROP TABLE IF EXISTS CovidDV;
+-- CREATE TABLE CovidDV AS
 
+
+-- 'SELECT * INTO new_table' worked for SSMS
+DROP TABLE IF EXISTS CovidDV;
+SELECT CovidVaccinations$.*,
+total_cases,
+new_cases,
+new_cases_smoothed,
+total_deaths,
+new_deaths,
+new_deaths_smoothed,
+total_cases_per_million,
+new_cases_per_million,
+new_cases_smoothed_per_million,
+total_deaths_per_million,
+new_deaths_per_million,
+new_deaths_smoothed_per_million,
+reproduction_rate,
+icu_patients,
+icu_patients_per_million,
+hosp_patients,
+hosp_patients_per_million,
+weekly_icu_admissions,
+weekly_icu_admissions_per_million,
+weekly_hosp_admissions,
+weekly_hosp_admissions_per_million 
+INTO CovidDV
+FROM CovidDeaths$ 
+JOIN CovidVaccinations$
+ON CovidDeaths$.location = CovidVaccinations$.location AND CovidDeaths$.date = CovidVaccinations$.date;
+
+-- Check the new table CovidDV
+SELECT *
+FROM CovidDV
 
 -- Total Population vs Vaccinations
 -- Shows Percentage of Population that has recieved at least one Covid Vaccine
-SELECT d.continent, d.location, d.date, d.population, v.new_vaccinations, 
-		SUM(CONVERT(int,v.new_vaccinations)) OVER (Partition by d.Location Order by d.location, d.Date) as RollingPeopleVaccinated
---, (RollingPeopleVaccinated/population)*100
-From PortfolioProject..CovidDeaths dea
-Join PortfolioProject..CovidVaccinations vac
-	On dea.location = vac.location
-	and dea.date = vac.date
-where dea.continent is not null 
-order by 2,3
+SELECT continent, location, date, population, new_vaccinations, 
+-- From the original code along, Alex use 'int' but the code won't work, we need to use 'BIGINT'
+	SUM(CONVERT(BIGINT, new_vaccinations)) OVER (PARTITION BY location ORDER BY location, date) as RollingPeopleVaccinated
+FROM CovidDV
+WHERE continent IS NOT NULL;
+
+-- Using CTE to perform Calculation on Partition By in previous query
+WITH PopvsVac AS
+(
+Select continent, location, date, population, new_vaccinations,
+		SUM(CONVERT(BIGINT, new_vaccinations)) OVER (PARTITION BY location ORDER BY location, date) as RollingPeopleVaccinated
+FROM CovidDV 
+WHERE continent IS NOT NULL
+)
+SELECT *, (RollingPeopleVaccinated/Population)*100 AS vac_pop_perc
+FROM PopvsVac;
+
+-- Using Temp Table to perform Calculation on Partition By in previous query
+DROP TABLE IF EXISTS PercentPopulationVaccinated
+CREATE TABLE PercentPopulationVaccinated
+(
+Continent nvarchar(255),
+Location nvarchar(255),
+Date datetime,
+Population numeric,
+New_vaccinations numeric,
+RollingPeopleVaccinated numeric
+)
+INSERT INTO PercentPopulationVaccinated
+Select continent, location, date, population, new_vaccinations,
+		SUM(CONVERT(BIGINT, new_vaccinations)) OVER (PARTITION BY location ORDER BY location, date) as RollingPeopleVaccinated
+FROM CovidDV
+WHERE continent IS NOT NULL;
+
+Select *, (RollingPeopleVaccinated/Population)*100 AS vac_pop_perc
+From PercentPopulationVaccinated
